570d352a1ba8da095da5c349369ff6539ce941bb
Alexandr <sanagl@mail.ru>
2022-05-20 22:42:08 +0300

    product

--- js/custom.js
+++ js/custom.js
@@ -143,6 +143,67 @@ $(function ($) {
 		}
 	});
 
+	$(".owl-related").owlCarousel({
+		//center:true,
+		//stagePadding: 280,
+		loop: true,
+		items: 3,
+		margin: 20,
+		//autoplay: false,
+		dots: false,
+		nav: true,
+		navText:
+		["<i class='news-str-left1 strelka' aria-hidden='true'><img src='img/ico/left1.svg'></i>",
+		"<i class='news-str-right1 strelka' aria-hidden='true'><img src='img/ico/right1.svg'></i>"],
+		/*responsive: {
+			0: {
+				//center:true,
+				stagePadding: 20,
+				items: 1,
+			},
+			370: {
+				center: true,
+				stagePadding: 50,
+				items: 1,
+			},
+			480: {
+				//center:true,
+				stagePadding: 100,
+				items: 1,
+			},
+			570: {
+				//center:true,
+				stagePadding: 120,
+				items: 1,
+			},
+			670: {
+				//center:true,
+				stagePadding: 15,
+				items: 2
+			},
+			992: {
+				stagePadding: 160,
+				items: 2,
+			},
+			1260: {
+				items: 2,
+			},
+
+			1450: {
+				stagePadding: 160,
+				items: 3,
+			},
+			1800: {
+				stagePadding: 320,
+				margin: 48,
+				items: 3,
+			},
+			2000: {
+				items: 3,
+			}
+		}*/
+	});
+
 })
 
 function theRotator() {
